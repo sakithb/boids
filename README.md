90d7f8472c159682953e64fef833992853b7fa94
Sakith <pvsakith@gmail.com>
2024-10-15 19:10:40 +0530

    added demo

---
+++ README.md
@@ -0,0 +1,6 @@
+### 5k boids
+![](demo/boids_5k.webm)
+
+### 10k boids
+![](demo/boids_10k.webm)
+
--- src/main.c
+++ src/main.c
@@ -115,7 +115,7 @@ int main(int argc, char **argv) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(scr_width, scr_height, "Conway's game of life", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(scr_width, scr_height, "Boids", NULL, NULL);
 	if (!window) {
 		fprintf(stderr, "failed to create window");
 		return -1;
